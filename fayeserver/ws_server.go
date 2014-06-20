82e2d616d579630066435738a5eab1d0abde5192
pcrawfor <pcrawfor@gmail.com>
2014-06-20 07:11:49 -0700

    Bumping up max message size

--- fayeserver/ws_server.go
+++ fayeserver/ws_server.go
@@ -45,7 +45,7 @@ const (
 	pingPeriod = (pongWait * 9) / 10
 
 	// Maximum message size allowed from peer.
-	maxMessageSize = 512
+	maxMessageSize = 1024
 )
 
 func (c *Connection) esWriter(f *FayeServer) {
